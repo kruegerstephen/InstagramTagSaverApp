58d937278ae710e92e46bde6b9527c70e07296a5
facchettos <facchetj@oregonstate.edu>
2017-06-08 16:48:49 -0700

    Preference screen

---
+++ app/src/main/java/com/example/tagsaver/SettingsFragment.java
@@ -0,0 +1,40 @@
+package com.example.tagsaver;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.preference.EditTextPreference;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+/**
+ * Created by hessro on 5/23/17.
+ */
+
+public class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState,String rootKey) {
+        addPreferencesFromResource(R.xml.prefs);
+        EditTextPreference userPref = (EditTextPreference)findPreference(getString(R.string.pref_user_key));
+        userPref.setSummary(userPref.getText());
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (key.equals(getString(R.string.pref_user_key))) {
+            EditTextPreference userPref = (EditTextPreference)findPreference(key);
+            userPref.setSummary(userPref.getText());
+        }
+    }
+}
