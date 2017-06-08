6a653bfcad4d6f5d18ba570d886431404201d968
NathanHealea <nate.healea@gmail.com>
2017-06-07 18:16:02 -0700

    new project structure

--- app/src/main/java/com/example/jeremy/tagsaver/RecyclerAdapter.java
+++
@@ -1,9 +0,0 @@
-package com.example.jeremy.tagsaver;
-
-/**
- * Created by Jeremy on 07-Jun-17.
- */
-
-public class RecyclerAdapter {
-
-}
---
+++ app/src/main/java/com/example/tagsaver/RecyclerAdapter.java
@@ -0,0 +1,9 @@
+package com.example.tagsaver;
+
+/**
+ * Created by Jeremy on 07-Jun-17.
+ */
+
+public class RecyclerAdapter {
+
+}
