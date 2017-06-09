4c090c095f9943ce4db5f60783acf7905c7a32ce
NathanHealea <nate.healea@gmail.com>
2017-06-09 11:55:02 -0700

    added git ignore

---
+++ app/src/main/java/com/example/tagsaver/utils/ApplicationData.java
@@ -0,0 +1,12 @@
+package com.example.tagsaver.utils;
+
+/**
+ * Created by nathan on 6/9/17.
+ */
+
+public class ApplicationData {
+    public static final String CLIENT_ID = "0d2ac8b6b85d40d191daf04223d27680";
+    public static final String CLIENT_SECRET = "0e111444031f4ac78e6563a97b812d3f";
+    public static final String CALLBACK_URL = "http://web.engr.oregonstate.edu/~healean/";
+}
+
